f4364b0551b8c310720afdf92ff9ba444bc24d70
RobsonRRamos <you@example.com>
2023-07-02 19:46:35 -0300

    feat: create address

--- address/models.py
+++ address/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from user.models import User
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=100, blank=False)
+    number = models.IntegerField(blank=False)
+    districk = models.CharField(max_length=100, blank=False)
+    city = models.CharField(max_length=100, blank=False)
+    state = models.CharField(max_length=100, blank=False)
+    country = models.CharField(max_length=100, blank=False)
+    zip_code = models.IntegerField(blank=False)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
