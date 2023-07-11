d1791847071d9218e5337472284329828ed4d692
Leticia <leticiaqueiroga25@gmail.com>
2023-07-11 17:20:05 -0300

    feat: include DER png

--- order/models.py
+++ order/models.py
@@ -14,7 +14,7 @@ class Order(models.Model):
         max_length=50, choices=Status.choices, default=Status.DEFAULT
     )
     created_at = models.DateTimeField(auto_now_add=True)
-    # price = models.DecimalField(max_digits=10, decimal_places=2)
+
     user = models.ForeignKey(
         "user.User", on_delete=models.CASCADE, related_name="orders"
     )
